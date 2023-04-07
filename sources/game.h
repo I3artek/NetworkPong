0f628395f33ca19b5e054118ece6e3a5908f24f1
i3artek <bartekwzorek@onet.pl>
2023-04-08 00:25:09 +0200

    unnecessary files deleted

--- sources/game.h
+++ sources/game.h
@@ -5,10 +5,12 @@
 #ifndef MY_RAYLIB_GAME_GAME_H
 #define MY_RAYLIB_GAME_GAME_H
 
-#include "paddle.h"
-#include "ball.h"
-
 #define SCREEN_WIDTH (600)
 #define SCREEN_HEIGHT (400)
+
+#define BALL_RADIUS 20
+
+#define PADDLE_WIDTH 200
+#define PADDLE_HEIGHT 40
 
 #endif //MY_RAYLIB_GAME_GAME_H
